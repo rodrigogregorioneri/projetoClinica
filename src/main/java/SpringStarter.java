fd8f0be2bed34b96272b73e9003b35ec147cff17
rodrigogregorioneri <rodrigogregorioneri>
2017-04-03 22:41:08 +0000

    Classe que starta o Spring

---
+++ src/main/java/SpringStarter.java
@@ -0,0 +1,20 @@
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author Rodrigo Gregorio Neri
+ * @version 1.0
+ * @since 2017-04-03
+ *
+ */
+ 
+public class SpringStarter {
+
+	/**
+	 * Construtor
+	 **/
+	public SpringStarter (){
+	}
+
+}
