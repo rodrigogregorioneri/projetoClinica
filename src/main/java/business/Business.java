d9f6719e490cafedc29c5d848792f44ad3f806c3
rodrigogregorioneri <rodrigogregorioneri>
2017-04-10 19:30:08 +0000

    Classespara subir alterações

---
+++ src/main/java/business/Business.java
@@ -0,0 +1,21 @@
+package business;
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author Rodrigo Gregorio Neri
+ * @version 1.0
+ * @since 2017-04-10
+ *
+ */
+ 
+public class Business {
+
+	/**
+	 * Construtor
+	 **/
+	public Business (){
+	}
+
+}
